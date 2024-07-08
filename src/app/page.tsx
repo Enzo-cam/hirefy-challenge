ec9df9dc0ccdc13460627e2a1498eb1f207fa0e2
Enzo-cam <camerac09@gmail.com>
2024-07-08 18:39:01 -0300

    Feat: ReleasesNotesTimeline and AllReleasesNotes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,26 @@
-
 import releasesTimeline from '../utils/releasesTimeline.json'
+import releasesNotes from  '../utils/releasesNotes.json'
+import ReleaseNotesTimeline from '@/Components/ReleaseNotesTimeline';
+import AllReleasesNotes from '@/Components/AllReleasesNotes';
 
 export default function Home() {
-  console.log(releasesTimeline)
-  return (
-    <main className="flex min-h-screen flex-col px-7">
-      <h1 className='mt-32 text-h1' >
-        {releasesTimeline.releaseVersion}      
-      </h1>
+  const releasesTimelineData = releasesTimeline;
+  const releasesNotesData = releasesNotes;
 
+  return (
+    <main className="lg:grid lg:grid-cols-3 gap-4 px-7 lg:mx-32 min-h-screen">
+      <div className="col-span-2">
+        <ReleaseNotesTimeline
+          releasesData={releasesTimelineData}
+        />
+      </div>
+
+      {/* Componente AllReleasesNotes ocupando 1/3 del espacio */}
+      <div className="col-span-1">
+        <AllReleasesNotes 
+          releasesNotes={releasesNotesData}
+        />
+      </div>
     </main>
   );
 }

ad2b8587db877efe7276e652f24065bd977721c6
Enzo-cam <camerac09@gmail.com>
2024-07-08 18:26:33 -0300

    Feat: remove 'view more' button on desktop

--- src/Components/AllReleasesNotes.tsx
+++ src/Components/AllReleasesNotes.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export interface ReleaseNoteItem {
   version: string;
@@ -15,27 +15,42 @@ interface Props {
 }
 
 const AllReleasesNotes = ({ releasesNotes }: Props) => {
-    const [visibleCount, setVisibleCount] = useState(11); // Comienza mostrando 11 elementos
+    const [visibleCount, setVisibleCount] = useState(11); 
   
     const showMore = () => {
-      setVisibleCount(prevCount => prevCount + 11); // Incrementa el número de elementos mostrados
+      setVisibleCount(prevCount => prevCount + 11); 
     };
-  
+
+    useEffect(() => {
+      const updateVisibleCount = () => {
+        // Cuando supere el width de 768px se van a mostrar todos los elementos
+        if (window.innerWidth >= 768) {
+          setVisibleCount(releasesNotes.releaseNotes.length);
+        } else {
+          setVisibleCount(11);
+        }
+      };
+      window.addEventListener('resize', updateVisibleCount);
+      updateVisibleCount();
+      return () => {
+        window.removeEventListener('resize', updateVisibleCount);
+      };
+    }, [releasesNotes.releaseNotes.length]);
     return (
       <div className="my-4">
         <h1 className='text-release'>All releases notes</h1>
         {releasesNotes.releaseNotes.slice(0, visibleCount).map((releaseNote, index) => (
           <div key={index}>
-            <h1 className={`font-sans ${
+            <p className={`font-sans lg:text-descriptionDate ${
               releaseNote.version === '6.5' ? 'text-grisOscuro font-bold underline' : 'text-gris font-semibold'
-            } lg:text-title`}>
+            }`}>
               Release {releaseNote.version}: {releaseNote.title}
-            </h1>
+            </p>
           </div>
         ))}
         {visibleCount < releasesNotes.releaseNotes.length && (
           <button
-            className="text-grad-azulOscuro p-2 rounded-md mt-4 font-semibold flex items-center"
+            className="text-grad-azulOscuro p-2 rounded-md mt-4 font-semibold flex items-center lg:hidden"
             onClick={showMore}
           >
             View more <span className="ml-2">⬇️</span>

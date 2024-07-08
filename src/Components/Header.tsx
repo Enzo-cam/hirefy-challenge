5c52c7119adbe3c73afd703158dbb8cd99231348
Enzo-cam <camerac09@gmail.com>
2024-07-08 17:01:22 -0300

    Feat: header and fix hydration

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,9 +1,9 @@
-
+'use client'
 const Header = () => {
   return (
-    <header className="fixed top-0 left-0 right-0 bg-gradient-to-r from-grad-azulOscuro  via-grad-celeste to-grad-verdeAguaClaro py-6 px-7">
+    <div className="fixed top-0 left-0 right-0 bg-gradient-to-r from-grad-azulOscuro  via-grad-celeste to-grad-verdeAguaClaro py-6 px-7">
       <h1 className="text-banner text-white font-sans">Release Notes</h1>
-    </header>
+    </div>
   )
 }
 

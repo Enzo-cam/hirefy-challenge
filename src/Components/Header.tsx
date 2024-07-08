ce9ea1717a810e6dfd4d90410d852d3d2eb98481
Enzo-cam <camerac09@gmail.com>
2024-07-08 16:20:15 -0300

    Setup and configurations

---
+++ src/Components/Header.tsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+  return (
+    <header className="fixed top-0 left-0 right-0 bg-gradient-to-r from-grad-azulOscuro  via-grad-celeste to-grad-verdeAguaClaro py-6 px-7">
+      <h1 className="text-banner text-white font-sans">Release Notes</h1>
+    </header>
+  )
+}
+
+export default Header
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,19 +2,50 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{ts,jsx,tsx}',
+    './components/**/*.{ts,jsx,tsx}',
+    './app/**/*.{ts,jsx,tsx}',
+    './src/**/*.{ts,jsx,tsx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        'verdeBase': '#2DCAB1',
+        'gris': '#7D879C',
+        'grisOscuro': '#2B3445',
+        'black': '#000000',
+        'white': '#FFFFFF',
+        'verdeAgua': '#1CB59C',
+        'celesteClaro': '#D3FFF5',
+        'yellowChillon': '#FFB800',
+        'beige': '#FFF8E5',
+        'salmon': '#F56565',
+        'rosadoClaro': '#FFDFDF',
+        'grad-azulOscuro': '#0D5287',
+        'grad-azulClaro': '#187DCA',
+        'grad-celeste': '#05AACE',
+        'grad-verdeAgua': '#04D2C6',
+        'grad-verdeAguaClaro': '#26E4C7'
       },
+      fontFamily: {
+        sans: ['Open Sans', 'sans-serif'],
+      },
+      fontSize: {
+        banner: '50px',  
+        h1: '44px',
+        title: '36px',
+        release: ['25px', {fontWeight: 'bold'} ],  
+        description: ['14px', {fontWeight: 'semibold'} ],  
+        chip: ['14px', {fontWeight: 'bold'} ]  
+      },
+      backgroundImage: {
+        'gradient-to-r': 'linear-gradient(to right, #007CF0, #00DFD8)',
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+      }
     },
   },
   plugins: [],
 };
+
 export default config;
